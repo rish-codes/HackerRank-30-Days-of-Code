4ba14698628dcd1f25f5afc372993a05c6a1b98f
rish-codes <67182874+rish-codes@users.noreply.github.com>
2020-07-12 08:54:36 +0530

    Day 1: Data Types

---
+++ Day1.py
@@ -0,0 +1,11 @@
+# Read and save an integer, double, and String to your variables.
+a = int(input())
+b = float(input())
+c = input()
+# Print the sum of both integer variables on a new line.
+print (i+a)
+# Print the sum of the double variables on a new line.
+print(d+b)
+# Concatenate and print the String variables on a new line
+print(s+c)
+# The 's' variable above should be printed first.
